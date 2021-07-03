3ef3f9fd9855fdd5e10c746332d78c6e9735bae2
Min Cai <min.cai.china@gmail.com>
2021-07-03 12:47:51 +0800

    Update files.

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 # Install extra prerequisites:
 # sudo apt-get install python-six zlib1g-dev libpng-dev swig
 
-cd ../gem5
+export GEM5=$1 || gem5
+
+cd ../$GEM5
 
 time scons ./build/ARM/gem5.opt -j 15
 
--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-export HOME=/home/itecgo
-export FS=$HOME/Tools/gem5-full-system-files
-export M5_PATH=$FS/aarch-system-20170616
+export M5_PATH=/home/itecgo/Tools/gem5-full-system-files/aarch-system-20170616
 
-cd ../gem5
+export GEM5=$1 || gem5
+
+cd ../$GEM5
 
 ./build/ARM/gem5.opt configs/example/fs.py \
---bootloader=$HOME/Tools/gem5-full-system-files/aarch-system-20170616/binaries/boot_emm.arm64 \
---kernel=$HOME/Tools/gem5-full-system-files/aarch-system-20170616/binaries/vmlinux.vexpress_gem5_v1_64.20170616 \
+--bootloader=$M5_PATH/binaries/boot_emm.arm64 \
+--kernel=$M5_PATH/binaries/vmlinux.vexpress_gem5_v1_64.20170616 \
 --machine-type=VExpress_GEM5_V1 \
---dtb-file=$HOME/Tools/gem5-full-system-files/aarch-system-20170616/binaries/armv8_gem5_v1_1cpu.20170616.dtb \
---disk-image=$HOME/Tools/gem5-full-system-files/aarch-system-20170616/disks/aarch64-ubuntu-trusty-headless.img \
+--dtb-file=$M5_PATH/binaries/armv8_gem5_v1_1cpu.20170616.dtb \
+--disk-image=$M5_PATH/disks/aarch64-ubuntu-trusty-headless.img \
 --cpu-type=O3_ARM_v7a_3 --caches \
 -r 1 \
 -I 1000000
