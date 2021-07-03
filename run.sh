e63ccf44d14674e02478fae1e3919cbf25417148
Min Cai <min.cai.china@gmail.com>
2021-07-03 17:28:14 +0800

    Update run.sh.

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ export M5_PATH=/home/itecgo/Tools/gem5-full-system-files/aarch-system-20170616
 --kernel=$M5_PATH/binaries/vmlinux.vexpress_gem5_v1_64.20170616 \
 --machine-type=VExpress_GEM5_V1 \
 --dtb-file=$M5_PATH/binaries/armv8_gem5_v1_1cpu.20170616.dtb \
---disk-image=$M5_PATH/disks/aarch64-ubuntu-trusty-headless.img \
+--disk-image=$M5_PATH/disks/expanded-linaro-minimal-aarch64.img \
 --cpu-type=O3_ARM_v7a_3 --caches \
 -r 1 \
 -I 1000000
