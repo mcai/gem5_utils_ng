50bfe9b443ce507318227ca0dfb6867fe37c0c36
Min Cai <min.cai.china@gmail.com>
2021-07-03 12:53:06 +0800

    Update files.

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 # Install extra prerequisites:
 # sudo apt-get install python-six zlib1g-dev libpng-dev swig
 
-export GEM5=$1
-
-cd ../$GEM5
-
 time scons ./build/ARM/gem5.opt -j 15
-
-cd ../gem5_utils_ng
--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 export M5_PATH=/home/itecgo/Tools/gem5-full-system-files/aarch-system-20170616
 
-export GEM5=$1
-
-cd ../$GEM5
-
 ./build/ARM/gem5.opt configs/example/fs.py \
 --bootloader=$M5_PATH/binaries/boot_emm.arm64 \
 --kernel=$M5_PATH/binaries/vmlinux.vexpress_gem5_v1_64.20170616 \
@@ -13,5 +9,3 @@ cd ../$GEM5
 --cpu-type=O3_ARM_v7a_3 --caches \
 -r 1 \
 -I 1000000
-
-cd ../gem5_utils_ng
