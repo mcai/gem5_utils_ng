9a5ddfa13db7b11a61a89abe5c8469d770a32f95
Min Cai <min.cai.china@gmail.com>
2021-07-03 18:06:20 +0800

    Update run.sh.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 scons ./build/ARM/gem5.opt -j 15
 
-export M5_PATH=/home/itecgo/Tools/gem5-full-system-files/aarch-system-20170616
+export M5_PATH=/home/itecgo/Tools/gem5-full-system-files
 
 ./build/ARM/gem5.opt configs/example/fs.py \
 --bootloader=$M5_PATH/binaries/boot_emm.arm64 \
@@ -11,7 +11,6 @@ export M5_PATH=/home/itecgo/Tools/gem5-full-system-files/aarch-system-20170616
 --machine-type=VExpress_GEM5_V1 \
 --dtb-file=$M5_PATH/binaries/armv8_gem5_v1_1cpu.20170616.dtb \
 --disk-image=$M5_PATH/disks/expanded-linaro-minimal-aarch64.img \
---script=/home/root/benchmarks/CPU2006/462.libquantum/462.libquantum.rcS \
+--script=$M5_PATH/benchmarks/CPU2006/462.libquantum/462.libquantum.rcS \
 --cpu-type=O3_ARM_v7a_3 --caches \
--r 1 \
--I 1000000
+-r 1
