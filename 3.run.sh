9fc797393e81ccb71842c686b1846c204aa6dd0a
Min Cai <min.cai.china@gmail.com>
2021-07-12 19:28:26 +0800

    Add docker files.

--- 3.run.sh
+++ 3.run.sh
@@ -1,9 +1,14 @@
 export M5_PATH=/gem5-full-system-files
 
-./build/ARM/gem5.opt configs/example/fs.py \
+export TIMESTAMP=$(date +%Y%d%m_%H%M%S)
+
+./build/ARM/gem5.opt \
+-d m5out/$TIMESTAMP/ \
+configs/example/fs.py \
 --kernel=$M5_PATH/binaries/vmlinux.arm64 \
 --disk-image=$M5_PATH/ubuntu-18.04-arm64-docker.img \
 --dtb-file=./system/arm/dt/armv8_gem5_v1_1cpu.dtb \
 --bootloader=$M5_PATH/binaries/boot.arm64 \
 --script=$M5_PATH/benchmarks/CPU2006/462.libquantum/462.libquantum.rcS \
---cpu-type=O3_ARM_v7a_3 --caches -r 1
+--cpu-type=O3_ARM_v7a_3 --caches \
+-r 1 --checkpoint-dir=m5out/
